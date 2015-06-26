13b666bea85930d665f1d8b9aa8e145eb00417a3
Khan Thompson <khanage@gmail.com>
2015-06-26 16:06:19 +1000

    Remove artefacts

--- psc.js
+++
@@ -1,7 +0,0 @@
-
-events.js:72
-        throw er; // Unhandled 'error' event
-              ^
-Error: spawn ENOENT
-    at errnoException (child_process.js:980:11)
-    at Process.ChildProcess._handle.onexit (child_process.js:771:34)
